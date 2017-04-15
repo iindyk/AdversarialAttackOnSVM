cc90d6cea9cd540821067b4018cc07346c6a6d58
iindyk <ihor.indyk@gmail.com>
2017-04-14 20:34:57 -0400

    basic linear svm built

--- data_file_clean.py
+++ data_file_clean.py
@@ -2,6 +2,6 @@
 f1 = open('data.txt', 'r')
 f2 = open('data_clean.txt', 'w')
 for line in f1:
-    f2.write(line.replace('1:', '').replace('2:', ''))
+    f2.write(line.replace(' 1:', ' ').replace(' 2:', ' '))
 f1.close()
 f2.close()
