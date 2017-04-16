bd46efd44bf380c541edd6cf90727d63b6a431fc
iindyk <ihor.indyk@gmail.com>
2017-04-15 22:09:48 -0400

    synthetic data for classification built, error rate estimate added

--- synthetic_data_test.py
+++ synthetic_data_test.py
@@ -1,7 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+
 from sklearn import svm
-from random import uniform
+from random import uniform, randint
+from sklearn.metrics import accuracy_score
 
 dataset = []
 labels = []
@@ -10,21 +13,36 @@ for i in range(0, 600):
     x1 = uniform(0, 200)
     x2 = uniform(0, 200)
     dataset.append([x1, x2])
-    if x2 >= x1-10:
+    if x2 >= x1 - 10:
         labels.append(1)
         colors.append((1, 0, 0))
     else:
         labels.append(-1)
         colors.append((0, 0, 1))
 
+true_labels = labels[:]
+# random attack
+for i in range(0, 60):
+    n = randint(0, 200)
+    if colors[n] != (0, 1, 0):
+        colors[n] = (0, 1, 0)
+        if labels[n] == 1:
+            labels[n] = -1
+        else:
+            labels[n] = 1
+
 C = 1.0  # SVM regularization parameter
-h = 1 # step size in the mesh
+h = 1  # step size in the mesh
 svc = svm.SVC(kernel='linear', C=C).fit(dataset, labels)
 x_min, x_max = 0, 200
 y_min, y_max = 0, 200
 xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
                      np.arange(y_min, y_max, h))
 Z = svc.predict(np.c_[xx.ravel(), yy.ravel()])
+
+predicted_labels = svc.predict(dataset)
+acc = accuracy_score(true_labels, predicted_labels)
+err = 1 - acc
 
 # Put the result into a color plot
 Z = Z.reshape(xx.shape)
@@ -38,6 +56,7 @@ plt.xlim(xx.min(), xx.max())
 plt.ylim(yy.min(), yy.max())
 plt.xticks(())
 plt.yticks(())
-plt.title('linear svm')
+plt.title('linear svm, err=' + str(err))
 
+print(len(dataset))
 plt.show()
