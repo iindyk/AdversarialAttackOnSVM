1afd8de8624a0e3b54c77159425897c79edadb85
iindyk <ihor.indyk@gmail.com>
2017-04-16 16:44:56 -0400

    self made svm added

--- my_svm.py
+++ my_svm.py
@@ -1,6 +1,7 @@
-from cvxopt import matrix, solvers
-from random import uniform
 import numpy as np
+from cvxopt import matrix, solvers
+
+from random import uniform
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 
@@ -15,36 +16,37 @@ for i in range(0, n):
     x2 = uniform(0, 100)
     dataset.append([x1, x2])
     if x2 >= x1 - 10:
-        labels.append(1)
+        labels.append(1.0)
         colors.append((1, 0, 0))
     else:
-        labels.append(-1)
+        labels.append(-1.0)
         colors.append((0, 0, 1))
 
 true_labels=labels[:]
 
 q_list = []
-g_list = [[0 for j in range(0, n)] for k in range(0, 2*n)]
+g_list = [[0 for j in range(0, 2*n)] for k in range(0, n)]
 h_list = [0 for l in range(0, 2*n)]
+
 for i in range(0, n):
     q_list.append([])
     for j in range(0, n):
         if i == j:
             q_list[i].append(np.dot(dataset[i], dataset[i]) * (labels[i] ** 2))
-            g_list[i][i] = -1
-            g_list[i+n][i] = 1
+            g_list[i][i] = -1.0
+            g_list[i][i+n] = 1.0
             h_list[i+n] = 1/(2*la*n)
         else:
-            q_list[i].append(0.5*np.dot(dataset[i], dataset[j]*labels[i]*labels[j]))
+            q_list[i].append(0.5*np.dot(dataset[i], dataset[j])*labels[i]*labels[j])
 
 Q = 2*matrix(q_list)
-p = matrix([-1 for i in range(0, n)])
+p = matrix([-1.0 for i in range(0, n)])
 G = matrix(g_list)
 h = matrix(h_list)
-A = matrix(labels)
-b = matrix(0)
+A = np.transpose(matrix(labels))
+b = matrix(0.0)
 
-sol = solvers.qp(Q, p, G, h, A, b)
+sol = solvers.qp(matrix(Q), p, matrix(G), h, matrix(A), b)
 c = sol['x']
 w = [0, 0]
 for i in range(0, n):
@@ -55,17 +57,22 @@ b = labels[0] - np.dot(w, dataset[0])
 
 h = 1  # step size in the mesh
 
-x_min, x_max = 0, 200
-y_min, y_max = 0, 200
+x_min, x_max = 0, 5
+y_min, y_max = 0, 5
 xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
                      np.arange(y_min, y_max, h))
-Z = np.sign(np.c_[np.dot([xx.ravel(), yy.ravel()], w)-b])
+Z_list = [[0.0 for j in range(x_min, x_max)] for r in range(y_min, y_max)]
+for i in range(x_min, x_max):
+    for j in range(y_min, y_max):
+        Z_list[i][j] = np.sign(xx[i]*w[0] + yy[j]*w[1]-b)
+print(Z_list)
 
-predicted_labels = np.sign((np.dot(dataset[i], w)-b) for i in range(0, n))
+predicted_labels = np.sign([np.dot(dataset[i], w)-b for i in range(0, n)])
 acc = accuracy_score(true_labels, predicted_labels)
 err = 1 - acc
 
 # Put the result into a color plot
+Z = np.array(Z_list)
 Z = Z.reshape(xx.shape)
 plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm, alpha=0.8)
 
