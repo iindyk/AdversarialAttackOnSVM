7d0b38b3687ecffc09c1e397e16b0c9b302cdf00
iindyk <ihor.indyk@gmail.com>
2017-04-29 00:30:20 -0400

    stochastic svm test added

--- stochastic_svm_test.py
+++ stochastic_svm_test.py
@@ -1,48 +1,72 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from scipy.optimize import minimize
 
-from sklearn import svm
-from random import uniform, randint
+from random import uniform
 from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
 
 dataset = []
 labels = []
 colors = []
-for i in range(0, 600):
+n = 600
+la = 0.8
+
+for i in range(0, n):
     x1 = uniform(0, 200)
     x2 = uniform(0, 200)
     dataset.append([x1, x2])
     if x2 >= x1 - 10:
-        labels.append(1)
+        labels.append(1.0)
         colors.append((1, 0, 0))
     else:
-        labels.append(-1)
+        labels.append(-1.0)
         colors.append((0, 0, 1))
 
 true_labels = labels[:]
-# random attack
-'''for i in range(0, 60):
-    n = randint(0, 200)
-    if colors[n] != (0, 1, 0):
-        colors[n] = (0, 1, 0)
-        if labels[n] == 1:
-            labels[n] = -1
-        else:
-            labels[n] = 1
-'''
-C = 1.0  # SVM regularization parameter
+# w[2]=b
+
+
+def objective(ww):
+    f = 0.0
+    for l in range(0, n):
+       f += min(1, labels[l]*(dataset[l][0]*ww[0]+dataset[l][1]*ww[1]+ww[0]))
+    return -f/n
+
+
+def constraint1(ww):
+    return ww[0]+ww[1]-1
+
+#
+# optimize
+b = (-200, 200)
+x0 = (0.5, 0.5, 100)
+bnds = (b, b, b)
+con1 = {'type': 'eq', 'fun': constraint1}
+cons = ([con1])
+solution = minimize(objective, x0, bounds=bnds, constraints=cons)
 h = 1  # step size in the mesh
-svc = svm.SVC(kernel='linear', C=C).fit(dataset, labels)
+
+w = [0, 0]
+w[0] = solution[0]
+w[1] = solution[1]
+b = solution[2]
+
 x_min, x_max = 0, 200
 y_min, y_max = 0, 200
 xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
                      np.arange(y_min, y_max, h))
-Z = svc.predict(np.c_[xx.ravel(), yy.ravel()])
+Z_list = []
+for i in range(x_min, x_max):
+    for j in range(y_min, y_max):
+        Z_list.append(np.sign(xx[i][j]*w[0] + yy[i][j]*w[1]+b))
 
-predicted_labels = svc.predict(dataset)
+predicted_labels = np.sign([np.dot(dataset[i], w)+b for i in range(0, n)])
 acc = accuracy_score(true_labels, predicted_labels)
 err = 1 - acc
+
 # Put the result into a color plot
+
+Z = np.array(Z_list)
 Z = Z.reshape(xx.shape)
 plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm, alpha=0.8)
 
@@ -54,7 +78,6 @@ plt.xlim(xx.min(), xx.max())
 plt.ylim(yy.min(), yy.max())
 plt.xticks(())
 plt.yticks(())
-plt.title('linear svm, err=' + str(err))
+plt.title('stochastic svm, err=' + str(err))
 
-print(len(dataset))
 plt.show()
