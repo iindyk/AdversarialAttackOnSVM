4be6a91d07de919fea6a4b31bea43319b81aa4f8
iindyk <ihor.indyk@gmail.com>
2017-04-30 23:45:25 -0400

    comparison analysis, data_read, missing features handling added

--- read_data.py
+++ read_data.py
@@ -2,15 +2,27 @@ import numpy as np
 
 
 def read_data(path):
-    data_str = np.genfromtxt(path, delimiter=" ", dtype=None)
+    m = int(input('number of features?'))  # features number!!!!!
+    f1 = open(path, 'wt')
+    f2 = open("data_clean.txt", 'w')
+    for line in f1:
+        cline = line[:]
+        d = line.count(' ')
+        for k in range(0, m-d):
+            cline += ' \' \''
+        f2.write(cline)
+
+    f1.close()
+    f2.close()
+    data_str = np.genfromtxt("data_clean.txt", delimiter=" ", dtype=None)
     data = []
     n = data_str.size
-    m = int(input('number of features?'))   # features number!!!!!
+
     for i in range(0, n):
         data.append([])
         data[i].append(float(data_str[i][0]))
         for j in range(1, m):
-            if str(j)+':' in data_str[i][j]:
+            if 'j'+':' in data_str[i][j]:
                 data[i].append(float(data_str[i][j].replace(str(j)+':', '')))
             else:
                 data[i].append(np.nan)
