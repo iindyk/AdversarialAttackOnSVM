4b8f31a2b617aacbd91ee4ec2e30e4639179bf7e
iindyk <ihor.indyk@gmail.com>
2017-05-23 00:25:03 -0400

    monte carlo attack simulation added

---
+++ new_ideas_0517/mc_adversarial_attack_modelling.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+from random import uniform
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+
+dataset = []
+labels = []
+colors = []
+n = 200  # training set size (must be larger than m to avoid fuck up)
+m = 2  # features
+C = 1.0  # SVM regularization parameter
+a = 20  # random attack size
+A = 10
+B = 110
+eps_list = []  # upper bound for norm of h
+h_list = [[0.0 for i in range(0, m)] for j in range(0, n)]  # attacks, first is zero attack
+nsim = 100  # number of simulations
+for i in range(0, n):
+    point = []
+    for j in range(0, m):
+        point.append(uniform(A, B))
+    dataset.append(point)
+    # change
+    if sum([p**2 for p in point]) >= 75**2:
+        labels.append(1.0)
+        colors.append((1, 0, 0))
+    else:
+        labels.append(-1.0)
+        colors.append((0, 0, 1))
+
+# random attack
+for i in range(0, a):
+    if labels[i] == 1:
+        labels[i] = -1
+        colors[i] = (0, 0, 1)
+    else:
+        labels[i] = 1
+        colors[i] = (1, 0, 0)
+
+# generating random attacks
+temp = np.random.normal(0.0, 1.0, nsim*n*2)
+for i in range(0, n*nsim):
+    t = np.array([])
+    nrm = 0.0
+    for j in range(0, m):
+        t = np.append(t, temp[i + j * nsim * n])
+        nrm += temp[i + j * nsim * n]**2
+    h_list.append([t[k]/nrm**0.5 for k in range(0, m)])
+# separation on nsim lists
+h_d_list = []
+for i in range(0, nsim):
+    h_d_list.append([h_list[i + j * nsim] for j in range(0, n)])
+# c-svm optimization
+errs = []
+for e in eps_list:
+    for h in h_d_list:
+        infected_dataset = [[dataset[j][i]+e*h[j][i] for i in range(0, m)] for j in range(0, n)]
+        svc = svm.SVC(kernel='linear', C=C).fit(infected_dataset, labels)
+        predicted_labels = svc.predict(dataset)
+        errs.append(1 - accuracy_score(labels, predicted_labels))
+
+print(errs)
