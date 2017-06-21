7d16b9176b4be8afc1bf775a6536edf9d14a3861
iindyk <ihor.indyk@gmail.com>
2017-06-21 10:07:44 -0400

    mixed problem with slack variables added

--- new_ideas_0517/attack_hard_margin_slack.py
+++ new_ideas_0517/attack_hard_margin_slack.py
@@ -16,7 +16,7 @@ C = 1.0/n  # SVM regularization parameter
 flip_size = 0  # random attack size
 A = 0
 B = 100
-eps = 0.1*(B-A)  # upper bound for (norm of h)**2
+eps = 0.5*(B-A)  # upper bound for (norm of h)**2
 maxit = 50
 delta = 1e-2
 ###############
@@ -30,11 +30,11 @@ for i in range(0, n):
     if sum([p for p in point]) >= m*(B-A)/2:
         labels.append(1.0)
         colors.append((1, 0, 0))
-        point[0] += 10
+        point[0] += 2
     else:
         labels.append(-1.0)
         colors.append((0, 0, 1))
-        point[0] -= 10
+        point[0] -= 2
     dataset.append(point)
 # random attack
 for i in range(0, flip_size):
@@ -51,25 +51,27 @@ print('err on orig is '+str(err_orig))
 
 
 def adv_obj(x):
-    return -sum(x[m+n+1:m+2*n+1])
+    return -sum(x[m+n+1:m+2*n+1]) + n*np.dot(x[:m], x[:m])
 
 
-def class_constr_inf_ineq(x, w_prev):
+def class_constr_inf_ineq(x, w_prev, ub):
     ret = []
     for i in range(0, n):
         ret.append(labels[i]*(np.dot(x[:m], dataset[i]) + x[m+1+i]+x[m])-1.0)
         ret.append(-labels[i]*(np.dot(x[:m], dataset[i]) + x[m]) + 1.0 - x[m+n+1+i])
     ret.append(eps*np.dot(w_prev, w_prev) - np.dot(x[m+1:m+n+1], x[m+1:m+n+1])/n)
     ret.append(np.dot(x[:m], w_prev) - 0.01)
+    ret.append(ub - (w_prev[0] - x[0])**2 - (w_prev[1] - x[1])**2)
     return ret
 
 x_opt = [0.05 for i in range(0, m+1+2*n)]
 options = {'maxiter': 100000}
 nit = 0
+ub = 1
 while nit < maxit:
     print('iteration ' + str(nit))
     w_p = x_opt[:m]
-    con = {'type': 'ineq', 'fun': class_constr_inf_ineq, 'args': [w_p]}
+    con = {'type': 'ineq', 'fun': class_constr_inf_ineq, 'args': [w_p, ub]}
     cons = [con]
     sol = minimize(adv_obj, np.array(x_opt), constraints=cons, options=options, method='COBYLA')
     print('success: '+str(sol.success))
@@ -85,6 +87,7 @@ while nit < maxit:
     if np.linalg.norm([w[i]-w_p[i] for i in range(0, m)]) < delta and sol.success:
         break
     nit += 1
+    ub /= 2
 
 #  restoring h
 #  h[:n] - h1s, h[n:2*n] - h2s
@@ -110,7 +113,7 @@ print(solution_h.nit)
 h = solution_h.x
 
 dataset_infected = []
-print(class_constr_inf_ineq(x_opt, w_p))
+print(class_constr_inf_ineq(x_opt, w_p, ub))
 print('attack norm= '+str(np.dot(h, h)/n))
 print('objective value= '+str(sol.fun))
 for i in range(0, n):
