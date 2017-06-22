c8deb49fc5cdeea6c9c6846a0f0e1292f317bfde
iindyk <ihor.indyk@gmail.com>
2017-06-22 12:29:35 -0400

    mixed problem with slack variables added

--- new_ideas_0621/mixed_attack_slack.py
+++ new_ideas_0621/mixed_attack_slack.py
@@ -10,16 +10,16 @@ import matplotlib.pyplot as plt
 dataset = []
 labels = []
 colors = []
-n = 100  # training set size (must be larger than m to avoid fuck up)
+n = 20  # training set size (must be larger than m to avoid fuck up)
 m = 2  # features
 C = 1.0/n  # SVM regularization parameter
-flip_size = 20  # random attack size
+flip_size = 2  # random attack size
 A = 0
 B = 100
-eps = 0.5*(B-A)  # upper bound for (norm of h)**2
+eps = 0.1*(B-A)  # upper bound for (norm of h)**2
 maxit = 50
 delta = 1e-5
-alpha = 5  # classifier objective weight
+alpha = 1  # classifier objective weight
 ###############
 #  x[:m]=w; x[m]=b; x[m+1:m+n+1]=g; x[m+n+1:m+2*n+1]=psi; x[m+2*n+1:m+3*n+1]=xi
 ###############
@@ -60,7 +60,7 @@ def adversary_obj(x):
 
 
 def classifier_obj(x):
-    return sum(x[m+2*n+1:m+3*n+1]) + C*np.dot(x[:m], x[:m])
+    return C*sum(x[m+2*n+1:m+3*n+1]) + np.dot(x[:m], x[:m])
 
 
 def class_constr_inf_ineq(x, w_prev, ub):
@@ -70,7 +70,7 @@ def class_constr_inf_ineq(x, w_prev, ub):
         ret.append(-labels[i]*(np.dot(x[:m], dataset[i]) + x[m]) + 1.0 - x[m+n+1+i])
     ret.append(eps*np.dot(w_prev, w_prev)*n - np.dot(x[m+1:m+n+1], x[m+1:m+n+1]))
     ret.append(np.dot(x[:m], w_prev) - 0.01)
-    #ret.append(ub - (w_prev[0] - x[0])**2 - (w_prev[1] - x[1])**2)
+    ret.append(ub - (w_prev[0] - x[0])**2 - (w_prev[1] - x[1])**2)
     return np.array(ret)
 
 bnds = [(0.0, 10.0), (0.0, 10.0), (-100.0, 100.0)]
@@ -80,7 +80,7 @@ for i in range(0, 2*n):
     bnds.append((0, 1e10))
 
 x_opt = [0.05 for i in range(0, m+1+3*n)]
-options = {'maxiter': 2000, 'disp': True}
+options = {'maxiter': 20000, 'disp': True}
 nit = 0
 ub = 1
 while nit < maxit:
@@ -88,7 +88,7 @@ while nit < maxit:
     w_p = x_opt[:m]
     con = {'type': 'ineq', 'fun': class_constr_inf_ineq, 'args': [w_p, ub]}
     cons = [con]
-    sol = minimize(objective, np.array(x_opt), method='SLSQP', constraints=cons, bounds=bnds, options=options)
+    sol = minimize(objective, np.array(x_opt), method='COBYLA', constraints=cons, bounds=bnds, options=options)
     #print('success: '+str(sol.success))
     #print('message: '+str(sol.message))
     x_opt = sol.x[:]
@@ -97,8 +97,8 @@ while nit < maxit:
     g = x_opt[m+1:m+n+1]
     #print('nfev= '+str(sol.nfev))
     #print('maxcv= '+str(sol.maxcv))
-    #print('w= '+str(w))
-    #print('b= '+str(b))
+    print('w= '+str(w))
+    print('b= '+str(b))
     if np.linalg.norm([w[i]-w_p[i] for i in range(0, m)]) < delta and sol.success:
         break
     nit += 1
