gradient descent with projection added. attack norm bug persists

sgd_optimization/obj_con_functions_v2.py
@@ -91,7 +91,7 @@ def class_constr_all_eq(w, b, h, l, a, dataset, labels, eps, C):
 def class_constr_all_eq_trunc_matrix(w_prev, h_prev, l_prev, dataset, labels, eps, C):
     # x[:m]=2; x[m]=b; x[m+1:m+1+n*m]=h; x[m+1+n*m:m+1+n*(m+1)]=l; x[m+1+n*(m+1):m+1+n*(m+2)]=a
     n, m = np.shape(dataset)
-    A = np.zeros(m+2+3*n, m+(m+2)*n+1)
+    A = np.zeros((m+2+3*n, m+(m+2)*n+1))
     b = np.zeros(m+2+3*n)
     # w=\sum l_i * y_i *(x_i + h_i)
     for j in range(0, m):

sgd_optimization/optimize.py
@@ -26,9 +26,13 @@ def sgd_adv_class_sbs(dataset_full, labels_full, eps, C, batch_size=-1, maxit=10
     nit = 0
     n, m = np.shape(dataset_full)
     x = np.random.normal(1.0/n, 1.0 / (3.0 * n), m+1+n*(m+2))
+    x_prev = np.zeros(m+1+n*(m+2))
     if batch_size == -1: batch_size = len(dataset_full) / 10
-    while nit < maxit and np.linalg.norm(x-x_prev)>precision:
-        if info: print('Iteration ', nit, '; start at ', dt.now().time())
+    while nit < maxit and np.linalg.norm(x-x_prev) > precision:
+        if info:
+            print('Iteration ', nit, '; start at ', dt.datetime.now().time())
+            print('w = ', x[:m], 'b = ', x[m])
+            print('attack norm = ', np.dot(x[m+1:m+1+m*n], x[m+1:m+1+m*n]))
         # indices = random.sample(range(0, len(dataset_full)), batch_size)
         # dataset = [dataset_full[i] for i in indices]
         # labels = [labels_full[i] for i in indices]