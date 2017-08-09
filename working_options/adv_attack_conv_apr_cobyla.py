48bf014f7394400736f626a0d3e6708e361f5ed5
iindyk <ihor.indyk@gmail.com>
2017-08-09 19:32:10 -0400

    nonconvex adversary attack problem with jacobians added

--- working_options/adv_attack_conv_apr_cobyla.py
+++ working_options/adv_attack_conv_apr_cobyla.py
@@ -1,11 +1,12 @@
 import datetime
 import numpy as np
 from scipy.optimize import minimize
+import sgd_optimization.obj_con_functions_v1 as of1
 from random import uniform
 from sklearn import svm
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
-import sgd_optimization.obj_con_functions_v1 as of1
+
 
 
 dataset = []
@@ -47,11 +48,55 @@ predicted_labels = svc.predict(dataset)
 err_orig = 1 - accuracy_score(labels, predicted_labels)
 print('err on orig is '+str(err_orig))
 
-# initial guess
+
+def decompose_x(x):
+    return x[:m], x[m], x[m+1:m*n+m+1], \
+           x[m*n+m+1:(m+1)*n+m+1], x[(m+1)*n+m+1:(m+2)*n+m+1]   # w, b, h, l, a
+
+def class_constr_inf_eq(x, w_prev, l_prev):
+    ret = []
+    w, b, h, l, a = decompose_x(x)
+    for j in range(0, m):
+        ret.append(w[j] - sum([l_prev[i]*labels[i]*(dataset[i][j]+h[j*n+i]) for i in range(0, n)]))
+    ret.append(sum([l[i]*labels[i] for i in range(0, n)]))
+    for i in range(0, n):
+        hi = [h[j * n + i] for j in range(0, m)]
+        ret.append(l[i] - l_prev[i]*a[i] - l_prev[i]*labels[i]*(np.dot(w, dataset[i])+np.dot(w_prev, hi) + b))
+        ret.append(l_prev[i]*a[i] - C*a[i])
+    return np.array(ret)
+
+
+def class_constr_inf_eq_neg(x, w_prev, l_prev):
+    ret = []
+    w, b, h, l, a = decompose_x(x)
+    for j in range(0, m):
+        ret.append(w[j] - sum([l_prev[i] * labels[i] * (dataset[i][j] + h[j * n + i]) for i in range(0, n)]))
+    ret.append(sum([l[i] * labels[i] for i in range(0, n)]))
+    for i in range(0, n):
+        hi = [h[j * n + i] for j in range(0, m)]
+        ret.append(l[i] - l_prev[i] * a[i] - l_prev[i] * labels[i] * (np.dot(w, dataset[i]) + np.dot(w_prev, hi) + b))
+        ret.append(l_prev[i] * a[i] - C * a[i])
+    return -1.0*np.array(ret)
+
+
+def class_constr_inf_ineq(x, w_prev):
+    ret = []
+    w, b, h, l, a = decompose_x(x)
+    for i in range(0, n):
+        ret.append(l[i])
+        ret.append(C - l[i])
+        ret.append(a[i])
+        ret.append(labels[i]*(np.dot(w, dataset[i]) + np.dot(w_prev, [h[j * n + i] for j in range(0, m)])+b)-1+a[i])
+    ret.append(eps*n - np.dot(h, h))
+    #ret.append(1 - np.dot(w, w))
+    #ret.append(1 - err_orig - adv_obj(x))
+    return np.array(ret)
+
 l_opt = []
 h_opt = []
 for i in range(0, n):
     if i in svc.support_:
+        #l_opt.append(1.0/n)
         l_opt.append(svc.dual_coef_[0][list(svc.support_).index(i)]/labels[i])
     else:
         l_opt.append(0.0)
@@ -64,23 +109,25 @@ while nit < maxit:
     w_p = x_opt[:m]
     l_p = x_opt[m*n+m+1:(m+1)*n+m+1]
     con1 = {'type': 'ineq', 'fun': of1.class_constr_inf_eq_convex, 'args': [w_p, l_p, dataset, labels, C]}
-    con2 = {'type': 'ineq', 'fun': lambda x: -of1.class_constr_inf_eq_convex(x, w_p, l_p, dataset, labels, C)}
-    con3 = {'type': 'ineq', 'fun': of1.class_constr_inf_ineq_convex_cobyla, 'args': [w_p, dataset, labels, eps, C]}
+    con2 = {'type': 'ineq', 'fun': lambda x: -1*of1.class_constr_inf_eq_convex(x, w_p, l_p, dataset, labels, C)}
+    con3 = {'type': 'ineq', 'fun': class_constr_inf_ineq, 'args': [w_p]}
     cons = [con1, con2, con3]
     sol = minimize(of1.adv_obj, x_opt, args=(dataset, labels), constraints=cons, options=options, method='COBYLA')
     print('success: '+str(sol.success))
     print('message: '+str(sol.message))
     x_opt = sol.x[:]
-    w, b, h, l, a = of1.decompose_x(x_opt, m, n)
+    w, b, h, l, a = decompose_x(x_opt)
     print('nfev= '+str(sol.nfev))
-    # print('maxcv= '+str(sol.maxcv))
+    #print('maxcv= '+str(sol.maxcv))
     print('w= '+str(w))
     print('b= '+str(b))
     x_p = list(w_p) + [b] + list(h) + list(l_p) + list(a)
     if of1.adv_obj(x_p, dataset, labels) <= sol.fun+delta \
-            and -delta <= of1.class_constr_inf_eq_convex(x_p, w_p, l_p, dataset, labels, C).all() <= delta \
-            and of1.class_constr_inf_ineq_convex_cobyla(x_p, w_p, dataset, labels, eps, C).all() >= -delta \
+            and class_constr_inf_eq(x_p, w_p, l_p).all() >= -delta \
+            and class_constr_inf_eq(x_p, w_p, l_p).all() <= delta \
+            and class_constr_inf_ineq(x_p, w_p).all() >= -delta \
             and sol.success and np.dot(h, h) / n > eps - 0.1:
+            #and -delta <= of1.class_constr_inf_eq_convex(x_p, w_p, l_p, dataset, labels, C).all() <= delta \
         break
     nit += 1
 
@@ -92,6 +139,9 @@ for i in range(0, n):
     for j in range(0, m):
         tmp.append(dataset[i][j]+h[j*n+i])
     dataset_infected.append(tmp)
+
+#print(class_constr_eq_orig(x_opt, dataset_infected))
+#print(class_constr_ineq_orig(x_opt, dataset_infected))
 
 svc1 = svm.SVC(kernel='linear', C=C)
 svc1.fit(dataset_infected, labels)
