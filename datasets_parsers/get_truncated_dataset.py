2c3d0619b562b02e1c40bbb41f07b4eb024a1eda
iindyk <ihor.indyk@gmail.com>
2017-08-11 14:12:03 -0400

    convex approximations of adversary attack with dataset truncating by distance added

--- datasets_parsers/get_truncated_dataset.py
+++ datasets_parsers/get_truncated_dataset.py
@@ -2,9 +2,11 @@ import numpy as np
 from sklearn import svm
 
 
-def truncate(dataset, labels, colors, maxdist, C, eps, svc=None):
+def truncate_by_dist(dataset, labels, colors, maxdist, C, eps, svc=None):
     if svc is None:
         svc = svm.SVC(kernel='linear', C=C).fit(dataset, labels)
+    assert np.dot(svc.coef_[0], svc.coef_[0]) + svc.intercept_**2 > 0
+    assert len(svc.support_) > 0
     dataset_trunc = []
     labels_trunc = []
     colors_trunc = []
@@ -21,6 +23,13 @@ def truncate(dataset, labels, colors, maxdist, C, eps, svc=None):
     svc1 = svm.SVC(kernel='linear', C=C).fit(dataset_trunc, labels_trunc)
     w_trunc = list(svc1.coef_[0])
     b_trunc = svc1.intercept_
+    # generating pilot h
+    h = np.zeros(n_t*m)
+    for i in range(n_t):
+        for j in range(m):
+            h[n_t*j+i] = np.sign(np.dot(w_trunc, dataset_trunc[i])+b_trunc) * \
+                         (w_trunc[j]/np.sqrt(np.dot(w_trunc, w_trunc)))*(np.sqrt(eps_t))
+    # generating vector of Lagrange multipliers
     l_trunc = []
     for i in range(len(dataset_trunc)):
         if i in svc1.support_:
@@ -30,10 +39,48 @@ def truncate(dataset, labels, colors, maxdist, C, eps, svc=None):
         else:
             l_trunc.append(0.0)
     a_trunc = [1 - min(1, labels_trunc[i] * (np.dot(w_trunc, dataset_trunc[i]) + b_trunc)) for i in range(len(dataset_trunc))]
-    x_trunc = w_trunc + list(b_trunc) + list(np.ones(m*n_t)*np.sqrt(eps_t/(m*n_t))) + l_trunc + a_trunc
+    x_trunc = w_trunc + list(b_trunc) + list(h) + l_trunc + a_trunc
     return dataset_trunc, labels_trunc, colors_trunc, indices, x_trunc, eps_t
 
 
 def dist(point, w, b):
     assert len(point) == len(w)
     return abs(np.dot(w, point)+b)/np.sqrt(sum(a**2 for a in w))
+
+
+def truncate_get_support(dataset, labels, colors, C, eps, svc=None):
+    if svc is None:
+        svc = svm.SVC(kernel='linear', C=C).fit(dataset, labels)
+    assert np.dot(svc.coef_[0], svc.coef_[0]) + svc.intercept_**2 > 0
+    assert len(svc.support_) > 0
+    dataset_trunc = []
+    labels_trunc = []
+    colors_trunc = []
+    indices = list(svc.support_)
+    for i in indices:
+        dataset_trunc.append(dataset[i])
+        labels_trunc.append(labels[i])
+        colors_trunc.append(colors[i])
+    n_t, m = np.shape(dataset_trunc)
+    eps_t = eps*len(dataset)/n_t
+    svc1 = svm.SVC(kernel='linear', C=C).fit(dataset_trunc, labels_trunc)
+    w_trunc = list(svc1.coef_[0])
+    b_trunc = svc1.intercept_
+    # generating pilot h
+    h = np.zeros(n_t*m)
+    for i in range(n_t):
+        for j in range(m):
+            h[n_t*j+i] = np.sign(np.dot(w_trunc, dataset_trunc[i])+b_trunc) * \
+                         (w_trunc[j]/np.sqrt(np.dot(w_trunc, w_trunc)))*(np.sqrt(eps_t))
+    # generating vector of Lagrange multipliers
+    l_trunc = []
+    for i in range(len(dataset_trunc)):
+        if i in svc1.support_:
+            l_trunc.append(svc1.dual_coef_[0][list(svc1.support_).index(i)] / labels_trunc[i])
+        elif labels_trunc[i] * (np.dot(w_trunc, dataset_trunc[i]) + b_trunc) < 0:
+            l_trunc.append(C)
+        else:
+            l_trunc.append(0.0)
+    a_trunc = [1 - min(1, labels_trunc[i] * (np.dot(w_trunc, dataset_trunc[i]) + b_trunc)) for i in range(len(dataset_trunc))]
+    x_trunc = w_trunc + list(b_trunc) + list(h) + l_trunc + a_trunc
+    return dataset_trunc, labels_trunc, colors_trunc, indices, x_trunc, eps_t
--- working_options/adv_attack_conv_apr_cobyla.py
+++ working_options/adv_attack_conv_apr_cobyla.py
@@ -1,5 +1,4 @@
 import datetime
-
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import minimize
@@ -7,43 +6,31 @@ from sklearn import svm
 from sklearn.metrics import accuracy_score
 import sgd_optimization.obj_con_functions_v1 as of1
 from datasets_parsers.random_dataset_generator import generate_random_dataset as grd
-from datasets_parsers.get_truncated_dataset import truncate as trunc
+from datasets_parsers.get_truncated_dataset import truncate_by_dist as trunc
 
-n = 70  # training set size (must be larger than m to avoid fuck up)
+n = 200  # training set size
 m = 2  # features
 C = 1.0  # SVM regularization parameter
 flip_size = 0  # random attack size
-A = 0
-B = 100
+A = 0  # left end of interval for generating points
+B = 100  # right end of interval for generating points
 eps = 0.1*(B-A)  # upper bound for (norm of h)**2
-maxit = 20
-delta = 1e-2
+maxit = 20  # maximum number of iterations
+delta = 1e-2  # precision for break from iterations
+options = {'maxiter': 10000}  # solver options
+maxdist = 0.1*(B-A)  # maximum allowed distance for dataset truncating
+
+
 dataset, labels, colors = grd(n=n, m=m, a=A, b=B, attack=flip_size, read=False, write=False, sep='linear')
 svc = svm.SVC(kernel='linear', C=C).fit(dataset, labels)
 predicted_labels = svc.predict(dataset)
 err_orig = 1 - accuracy_score(labels, predicted_labels)
-print('err on orig is '+str(err_orig))
-
-
-'''w = list(svc.coef_[0])
-b = svc.intercept_
-l = []
-for i in range(n):
-    if i in svc.support_:
-        l.append(svc.dual_coef_[0][list(svc.support_).index(i)] / labels[i])
-    elif labels[i]*(np.dot(w, dataset[i])+b) < 0:
-        l.append(C)
-    else:
-        l.append(0.0)
-a = [1-min(1, labels[i]*(np.dot(w, dataset[i])+b)) for i in range(n)]
-x_opt = w + list(b)+list(np.zeros(m*n)) + l + a'''
-
-dataset_trunc, labels_trunc, colors_trunc, indices, x_opt, eps_t = trunc(dataset, labels, colors, 10, C, eps)
+print('err on orig is '+str(int(err_orig*100))+'%')
+dataset_trunc, labels_trunc, colors_trunc, indices, x_opt, eps_t = trunc(dataset, labels, colors, maxdist, C, eps)
 n_t = len(dataset_trunc)
 print('number of closest chosen points is '+str(n_t))
 w = x_opt[:m]
 l = x_opt[m+1+m*n_t:m+1+(m+1)*n_t]
-options = {'maxiter': 10000}
 nit = 0
 while nit < maxit:
     print('iteration '+str(nit)+'; start: '+str(datetime.datetime.now().time()))
@@ -60,7 +47,6 @@ while nit < maxit:
     x_opt = sol.x[:]
     w, b, h, l, a = of1.decompose_x(x_opt, m, n_t)
     print('nfev= '+str(sol.nfev))
-    #print('maxcv= '+str(sol.maxcv))
     print('w= '+str(w))
     print('b= '+str(b))
     print('attack_norm= '+str(100*np.dot(h, h)//(n_t*eps_t))+'%')
@@ -101,52 +87,54 @@ predicted_labels_inf_opt = np.sign([np.dot(dataset[i], w)+b for i in range(0, n)
 err_inf_opt = 1 - accuracy_score(labels, predicted_labels_inf_opt)
 print('err on infected dataset by opt is '+str(int(100*err_inf_opt))+'%')
 
-plt.subplot(321)
-plt.title('original')
-plt.scatter([float(i[0]) for i in dataset], [float(i[1]) for i in dataset], c=colors, cmap=plt.cm.coolwarm)
-plt.subplot(322)
-plt.title('infected')
-plt.scatter([float(i[0]) for i in dataset_infected], [float(i[1]) for i in dataset_infected], c=colors, cmap=plt.cm.coolwarm)
-plt.subplot(323)
-step = 1  # step size in the mesh
+# plots
+if m == 2:
+    plt.subplot(321)
+    plt.title('original')
+    plt.scatter([float(i[0]) for i in dataset], [float(i[1]) for i in dataset], c=colors, cmap=plt.cm.coolwarm)
+    plt.subplot(322)
+    plt.title('infected')
+    plt.scatter([float(i[0]) for i in dataset_infected], [float(i[1]) for i in dataset_infected], c=colors, cmap=plt.cm.coolwarm)
+    plt.subplot(323)
+    step = 1  # step size in the mesh
 
-x_min, x_max = int(A-2*(eps/m)**0.5), int(B+2*(eps/m)**0.5)
-y_min, y_max = int(A-2*(eps/m)**0.5), int(B+2*(eps/m)**0.5)
-xx, yy = np.meshgrid(np.arange(x_min, x_max, step),
-                     np.arange(y_min, y_max, step))
-Z = np.sign([i[0]*w[0]+i[1]*w[1]+b for i in np.c_[xx.ravel(), yy.ravel()]])
+    x_min, x_max = int(A-2*(eps/m)**0.5), int(B+2*(eps/m)**0.5)
+    y_min, y_max = int(A-2*(eps/m)**0.5), int(B+2*(eps/m)**0.5)
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, step),
+                         np.arange(y_min, y_max, step))
+    Z = np.sign([i[0]*w[0]+i[1]*w[1]+b for i in np.c_[xx.ravel(), yy.ravel()]])
 
-Z = Z.reshape(xx.shape)
-plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm, alpha=0.8)
-plt.scatter([float(i[0]) for i in dataset_infected], [float(i[1]) for i in dataset_infected], c=colors, cmap=plt.cm.coolwarm)
-plt.title('opt on inf data')
-plt.plot([i[0] for i in inf_points], [i[1] for i in inf_points], 'go', mfc='none')
-plt.subplot(324)
-xx, yy = np.meshgrid(np.arange(x_min, x_max, step),
-                     np.arange(y_min, y_max, step))
-Z = svc1.predict(np.c_[xx.ravel(), yy.ravel()])
-Z = Z.reshape(xx.shape)
-plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm, alpha=0.8)
-plt.scatter([float(i[0]) for i in dataset_infected], [float(i[1]) for i in dataset_infected], c=colors, cmap=plt.cm.coolwarm)
-plt.plot([i[0] for i in inf_points], [i[1] for i in inf_points], 'go', mfc='none')
-plt.title('inf svc on inf data')
+    Z = Z.reshape(xx.shape)
+    plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm, alpha=0.8)
+    plt.scatter([float(i[0]) for i in dataset_infected], [float(i[1]) for i in dataset_infected], c=colors, cmap=plt.cm.coolwarm)
+    plt.title('opt on inf data')
+    plt.plot([i[0] for i in inf_points], [i[1] for i in inf_points], 'go', mfc='none')
+    plt.subplot(324)
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, step),
+                         np.arange(y_min, y_max, step))
+    Z = svc1.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm, alpha=0.8)
+    plt.scatter([float(i[0]) for i in dataset_infected], [float(i[1]) for i in dataset_infected], c=colors, cmap=plt.cm.coolwarm)
+    plt.plot([i[0] for i in inf_points], [i[1] for i in inf_points], 'go', mfc='none')
+    plt.title('inf svc on inf data')
 
-plt.subplot(326)
-xx, yy = np.meshgrid(np.arange(x_min, x_max, step),
-                     np.arange(y_min, y_max, step))
-Z = svc1.predict(np.c_[xx.ravel(), yy.ravel()])
-Z = Z.reshape(xx.shape)
-plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm, alpha=0.8)
-plt.scatter([float(i[0]) for i in dataset], [float(i[1]) for i in dataset], c=colors, cmap=plt.cm.coolwarm)
-plt.title('inf svc on orig data')
+    plt.subplot(326)
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, step),
+                         np.arange(y_min, y_max, step))
+    Z = svc1.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm, alpha=0.8)
+    plt.scatter([float(i[0]) for i in dataset], [float(i[1]) for i in dataset], c=colors, cmap=plt.cm.coolwarm)
+    plt.title('inf svc on orig data')
 
-plt.subplot(325)
-xx, yy = np.meshgrid(np.arange(x_min, x_max, step),
-                     np.arange(y_min, y_max, step))
-Z = svc.predict(np.c_[xx.ravel(), yy.ravel()])
-Z = Z.reshape(xx.shape)
-plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm, alpha=0.8)
-plt.scatter([float(i[0]) for i in dataset], [float(i[1]) for i in dataset], c=colors, cmap=plt.cm.coolwarm)
-plt.title('orig svc on orig data')
-plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
-plt.show()
+    plt.subplot(325)
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, step),
+                         np.arange(y_min, y_max, step))
+    Z = svc.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm, alpha=0.8)
+    plt.scatter([float(i[0]) for i in dataset], [float(i[1]) for i in dataset], c=colors, cmap=plt.cm.coolwarm)
+    plt.title('orig svc on orig data')
+    plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
+    plt.show()
