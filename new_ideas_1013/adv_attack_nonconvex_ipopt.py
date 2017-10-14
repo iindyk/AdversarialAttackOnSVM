c85b5061e3c372b717cd17d73fb75d137e7b9323
iindyk <ihor.indyk@gmail.com>
2017-10-13 22:31:46 -0400

    nonconvex optimization problem for optimal attack solved with pyipopt

--- new_ideas_1013/adv_attack_nonconvex_ipopt.py
+++ new_ideas_1013/adv_attack_nonconvex_ipopt.py
@@ -2,7 +2,9 @@ import pyipopt
 import numpy as np
 import utils.ofs.obj_con_functions_v1 as of1
 from sklearn import svm
+from sklearn.metrics import accuracy_score
 from utils.datasets_parsers.random_dataset_generator import generate_random_dataset as grd
+from utils.graphs import graph_results as graph
 
 
 n = 20  # training set size
@@ -60,4 +62,40 @@ def eval_jac_g(x, flag):
             for j in range(nvar):
                 ret.append(jac[i, j])
         return np.array(ret)
+
+
+nlp = pyipopt.create(nvar, x_L, x_U, ncon, g_L, g_U, nnzj, nnzh, eval_f,
+                     eval_grad_f, eval_g, eval_jac_g)
+print("Going to call solve")
+x, zl, zu, constraint_multipliers, obj, status = nlp.solve(x0)
+nlp.close()
+print('status: ', status)
+
+# analysis of results:
+w, b, h, l, a = of1.decompose_x(x, m, n)
+dataset_infected = []
+print('attack norm= '+str(np.dot(h, h)/n))
+print('objective value= '+str(obj))
+k = 0
+for i in range(0, n):
+    tmp = []
+    for j in range(0, m):
+        tmp.append(dataset[i][j]+h[j*n+k])
+    dataset_infected.append(tmp)
+# define infected points for graph
+inf_points = []
+k = 0
+for i in range(n):
+    if sum([h[j*n+k]**2 for j in range(m)]) > 0.9*eps:
+        inf_points.append(dataset_infected[i])
+    k += 1
+svc_inf = svm.SVC(kernel='linear', C=C)
+svc_inf.fit(dataset_infected, labels)
+predicted_labels_inf_svc = svc_inf.predict(dataset)
+err_inf_svc = 1 - accuracy_score(labels, predicted_labels_inf_svc)
+print('err on infected dataset by svc is '+str(int(100*err_inf_svc))+'%')
+predicted_labels_inf_opt = np.sign([np.dot(dataset[i], w)+b for i in range(0, n)])
+err_inf_opt = 1 - accuracy_score(labels, predicted_labels_inf_opt)
+print('err on infected dataset by opt is '+str(int(100*err_inf_opt))+'%')
+graph(A, B, eps, dataset, dataset_infected, inf_points, colors, x, n, svc_orig, svc_inf)
 
